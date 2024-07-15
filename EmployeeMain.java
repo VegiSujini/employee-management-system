f31d44f3d2f570c8fbfa1c0012bd72c95441edfc
VegiSujini <sujini.vegi@gmail.com>
2024-07-15 22:00:25 +0530

    added comments

--- EmployeeMain.java
+++ EmployeeMain.java
@@ -3,24 +3,34 @@ import java.util.concurrent.TimeUnit;
 public class EmployeeMain {
 public static void main(String[] args) {
         EmployeeService employeeService = new EmployeeService();
+        
+        //Adding the employees
 
         employeeService.addEmployee(new Employee(1, "Sujini", 23, "Engineering"));
         employeeService.addEmployee(new Employee(2, "Bharath", 18, "HR"));
         employeeService.addEmployee(new Employee(3, "Sankar", 26, "Marketing"));
 
         try{
+
+            //Get employee by id
+
             TimeUnit.SECONDS.sleep(1);
             Employee emp1 = employeeService.getEmployeeById(2);
             System.out.println("Employee found: "+emp1.getName());
+
+            //Get all employees
 
             TimeUnit.SECONDS.sleep(1);
             System.out.println("All employees");
             employeeService.getAllEmployees().forEach(emp -> System.out.println(emp.getName()));
 
+            //Get employees by department
+
             TimeUnit.SECONDS.sleep(1);
             System.out.println("Engineering Employees: ");
             employeeService.getEmployeesByDepartment("Engineering").forEach(emp -> System.out.println(emp.getName()));
 
+            //Get employees order by sorting their names
 
             TimeUnit.SECONDS.sleep(1);
             System.out.println("Employees Sorted by name: ");
